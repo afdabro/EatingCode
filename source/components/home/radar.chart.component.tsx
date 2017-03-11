2ea0ccf29729b246c65b205b181d6a2dbc7d535a
afdabro <andrewfdabrowski@gmail.com>
2017-03-10 21:35:58 -0500

    Feature/d3 (#4)
    * Implemented basic D3 support with a simple radar chart

---
+++ source/components/home/radar.chart.component.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import * as rd3 from "react-d3-library";
+import { RadarChart } from "./radar.chart";
+import { RadarChartOptions } from "./radar.chart.options";
+
+// tslint:disable-next-line:variable-name
+const RD3Component = rd3.Component;
+
+// TODO: Get from data redux store
+const data = require("./career.experience.json");
+
+interface IRadarChartState {
+    // tslint:disable-next-line:no-any
+    d3: any;
+}
+
+export class RadarChartComponent extends React.Component<RadarChartOptions, IRadarChartState> {
+    private radarChart = new RadarChart();
+    private chartSettings: RadarChartOptions;
+    constructor(props) {
+        super(props);
+
+        // TODO: Migrate chart options to redux
+        const options = new RadarChartOptions();
+        Object.assign(options, props);
+        this.chartSettings = options;
+        this.state = { "d3": "" };
+    }
+
+    public componentDidMount() {
+        this.setState({ "d3": this.radarChart.draw(data, this.chartSettings) });
+    }
+
+    // TODO: work on verbiage
+    public render() {
+        return (
+            <figure>
+                <strong>Career Experience</strong>
+                <RD3Component data={this.state.d3} />
+                <figcaption>Fig. 1: <em>Self reflection of real-world experience.</em></figcaption>
+            </figure>
+        );
+    }
+}
